1193ec934672e021de3eb506d69880ce93886bf9
Touraj Khatibi <2rajpx@gmail.com>
2021-06-25 03:30:55 +0430

    Add time related info to boxes

--- src/components/PanicMonster/PanicMonster.tsx
+++ src/components/PanicMonster/PanicMonster.tsx
@@ -1,4 +1,5 @@
 import { times } from "lodash";
+import { calculateAge, getDateOfMultipleDaysLater } from "../../helpers/datetime-helpers";
 import { useStore } from "../../hooks/store";
 import "./PanicMonster.css";
 
@@ -6,22 +7,29 @@ export const YEARS = 90;
 
 export const YEAR_WEEKS = 52.14;
 
-export function PanicMonster({ years = YEARS }) {
-  const {
-    state: { birthday },
-  } = useStore();
-  const onClick = (w: number) => alert(`you clicked on week ${w}`);
+export const WEEKS = YEARS * YEAR_WEEKS + 4;
+
+export function PanicMonster() {
+  const { state } = useStore();
+  const now = new Date();
+  const dob = state.birthday ? new Date(state.birthday) : undefined;
   return (
     <div className="PanicMonster">
-      {birthday}
-      {times(Math.ceil(years * YEAR_WEEKS), (w) => (
-        <figure
-          key={w}
-          className="PanicMonster-week"
-          title={`${w + 1}`}
-          onClick={() => onClick(w + 1)}
-        ></figure>
-      ))}
+      {times(Math.ceil(WEEKS), (w) => {
+        const date = dob ? getDateOfMultipleDaysLater(dob, w * 7) : undefined;
+        const classes = ["PanicMonster-week"];
+        date && date < now && classes.push('PanicMonster-past-week');
+        const age = dob && date ? calculateAge(dob, date) : undefined;
+        const title = `#${w + 1}${date ? ` - ${date.toDateString()}\n${age}` : ''}`;
+        return (
+          <figure
+            key={w}
+            className={classes.join(' ')}
+            title={title}
+            onClick={() => alert(title)}
+          ></figure>
+        );
+      })}
     </div>
   );
 }
